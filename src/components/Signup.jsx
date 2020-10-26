62be2aefe1a82db59d0ed6c70cfe3fd339239967
Akshaj Mody <akshajmody@gmail.com>
2020-10-27 03:32:29 +0900

    Add basic user signup functionality

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
-  return (
-    <div>
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    </div>
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Password confirmation failed')
+    }
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Account creation failed')
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    //FRAGMENT
+    <>
+      <Card>
+        <Card.Body>
+          <h1 className="text-center mb-3">NETaurin</h1>
+          <h6 className="text-center mb-3">Join the book review rainforest today!</h6>
+          {JSON.stringify(currentUser.email)}
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log In
+      </div>
+    </>
   )
 }
