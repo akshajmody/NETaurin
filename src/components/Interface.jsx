a051097c390972c05f14484b708a56a4e3b48c7d
Akshaj Mody <akshajmody@gmail.com>
2020-10-29 11:27:57 +0900

    Render image in reviews component

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -34,21 +34,19 @@ export default function Interface() {
 
   let time = new Date().toLocaleTimeString('en-US');
 
-  //FOR LOOP FOR BOOK TITLES
-
   return (
     <>
-      <Card>
+      <Card className="interfaceCard">
         <Card.Body>
-          <h2 className="text-center mb-1 mr-5 ml-5">Welcome back <strong>{currentUser.email}! </strong></h2>
-          <div className="text-center">Today's bestsellers as of {time}:</div>
+          <h2 className="text-center mb-2 mr-5 ml-5">Welcome back <strong>{currentUser.email}! </strong></h2>
+          <div className="text-center mb-2">Today's bestsellers as of {time}:</div>
           <div className="bookList">
             {apiData.bestsellers ? apiData.bestsellers.slice(0,10).map((bestseller) =>
             <Link to={`/reviews/${bestseller.asin}`}>
               <Card key={bestseller.asin}> #{bestseller.rank}: {bestseller.title}
                 <Card.Img src={bestseller.image} alt="book image" />
               </Card>
-            </Link>) : ""}
+            </Link>) : <img className="loadImage" src="https://www.fpt-software.com/wp-content/themes/genesis_fpt-software/core/custom_ajax/loading2.gif" alt="loading"/>}
           </div>
           {error && <Alert variant="danger">{error}</Alert>}
         </Card.Body>
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
+import axios from 'axios';
 
 
 export default function Reviews() {
   const { asin } = useParams();
+  const [bookData, setBookData] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://api.rainforestapi.com/request?api_key=${process.env.REACT_APP_RF_API_KEY}&type=product&amazon_domain=amazon.com&asin=${asin}&output=json`)
+    .then((response) => {
+      setBookData(response.data)
+    })
+  }, [asin])
+
 
 
   //MAKE ANOTHER REQUEST BASED ON ASIN
 
+  // BOOK TEST PARAM {asin}
 
   return (
     <div>
-      BOOK TEST PARAM {asin}
+      {bookData.product ? <img src={bookData.product.main_image.link} alt="book images"/> : <div>LOADING</div>}
     </div>
   )
 }
+
+
